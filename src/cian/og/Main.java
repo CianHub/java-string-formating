bb9fdcf210af5f0d1f800c9c385865ff73d5c175
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-05 13:06:49 +0100

    Add currency formatting

--- src/cian/og/Main.java
+++ src/cian/og/Main.java
@@ -1,8 +1,11 @@
 package cian.og;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
 
         String apples = "Apples";
         int appleQuantity = 8;
@@ -15,9 +18,11 @@ public class Main {
         String col2Heading = "Quantity";
         String col3Heading = "Price";
 
-        System.out.printf("%-12s %8s %6s%n", col1Heading, col2Heading, col3Heading);
-        System.out.printf("%-12s %8d %6d cents%n", apples, appleQuantity, applePrice);
-        System.out.printf("%-12s %8d %6d cents%n", oranges, orangeQuantity, orangePrice);
+        System.out.printf("%-12s %-8s %6s%n", col1Heading, col2Heading, col3Heading);
+        System.out.printf("%-12s %-8d %6s%n", apples, appleQuantity,
+                currencyFormat.format(applePrice/ 100.0));
+        System.out.printf("%-12s %-8d %6s%n", oranges, orangeQuantity,
+                currencyFormat.format(orangePrice / 100.0));
 
     }
 }
